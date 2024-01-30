72fd2d57d63e50807a753d1c4cc099e528edf91e
Tayyab-103 <tayyabhameed103@gmail.com>
2024-01-30 11:24:43 +0500

    change Account Name

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 export const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
+  displayName: 'Fahad Tufail',
+  email: 'fahad@gmail.com',
   photoURL: '/assets/images/avatars/avatar_25.jpg',
 };
